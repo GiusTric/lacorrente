d866555a0a6de00a01e553925786a2c16c2d3a5e
GiusTric <gius.tricarico@gmail.com>
2021-04-02 17:28:26 +0200

    New

---
+++ js/init.js
@@ -0,0 +1,23 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+$('.slider').slider({
+    full_width: true,
+    interval: 1000
+});
+
+$('.slider').slider('pause');
+
+$('#nextBtn').click(function(){
+    $('.slider').slider('start');
+    setTimeout(function(){
+        $('.slider').slider('pause');
+    },800);
+});
